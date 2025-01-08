00e07195089b986aea62c8e078a140413c45ad1b
gowtham6477 <jgjg6477@gmail.com>
2025-01-08 21:41:13 +0530

    update users api

--- src/app/api/users/route.jsx
+++ src/app/api/users/route.jsx
@@ -1,14 +1,8 @@
-
-export const dynamic = 'force-dynamic'; // Disable pre-rendering
 import { User } from "@/app/models/User";
-import { connectDB } from '@/utils/db';
+import mongoose from "mongoose";
 
-export async function GET(req) {
-  try {
-    await connectDB(); // Connect to DB
-    const users = await User.find(); // Fetch users
-    return Response.json(users);
-  } catch (err) {
-    return Response.json({ error: err.message }, { status: 500 });
-  }
+export async function GET() {
+  mongoose.connect(process.env.MONGO_URL)
+  const users = await User.find()
+  return Response.json(users);
 }
